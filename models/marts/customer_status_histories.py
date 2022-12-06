0389cefc5d19d6649866d7cc14d707c66c021324
gwen windflower <gwenwindflower@gmail.com>
2022-12-06 09:53:48 -0600

    Update customer status history logic

--- models/marts/customer_status_histories.py
+++ models/marts/customer_status_histories.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 
 def model(dbt, session) -> pd.DataFrame:
+    # set length of time considered a churn
+    churn_span: pd.Timedelta = pd.Timedelta(days=2)
 
     dbt.config(materialized="table", packages=["pandas==1.5.2"])
 
@@ -12,24 +14,6 @@ def model(dbt, session) -> pd.DataFrame:
     orders_df["previous_order_at"] = orders_df.groupby("customer_id")[
         "ordered_at"
     ].shift(1)
-    orders_df["interval_between_orders"] = (
-        orders_df["ordered_at"] - orders_df["previous_order_at"]
-    )
-    orders_df["days_between_orders"] = (
-        orders_df["ordered_at"] - orders_df["previous_order_at"]
-    )
-    # TODO: flag most recent order
-    # TODO: calculate days since most recent order, per customer
-    # TODO: remember, this will be a customer-per-status grain when finished
-    # orders_df["days_since_last_order"] = (
-    #     pd.datetime.now() - orders_df["ordered_at"]
-    # )
-    # churn_span: pd.Timedelta = pd.Timedelta(days=2)
-    # if orders_df["days_between_orders"] is None:
-    #     orders_df["status"] = 'new'
-    # else:
-    #     if orders_df["days_between_orders"] > churn_span:
-
-    #     else:
-
+    orders_df["next_order_at"] = orders_df.groupby("customer_id")["ordered_at"].shift(
+        -1
     return orders_df
