63fe92acc8c70fa6ffa37f57a6a5038687ebd8b9
winnie <91998347+gwenwindflower@users.noreply.github.com>
2022-12-06 14:26:47 -0600

    Configure dev container (#1)
    * Initial config for Codespaces dev container
    
    * Update dev container config

--- models/marts/customer_status_histories.py
+++ models/marts/customer_status_histories.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
 
-def model(dbt, session) -> pd.DataFrame:
+def model(dbt, session):
+
     # set length of time considered a churn
-    churn_span: pd.Timedelta = pd.Timedelta(days=2)
+    churn_span = pd.Timedelta(days=2)
 
     dbt.config(materialized="table", packages=["pandas==1.5.2"])
 
     orders_relation = dbt.ref("stg_orders")
-    orders_df: pd.DataFrame = orders_relation.df()
+
+    # converting a DuckDB Python Relation into a pandas DataFrame
+    orders_df = orders_relation.df()
 
     orders_df.sort_values(by="ordered_at", inplace=True)
     orders_df["previous_order_at"] = orders_df.groupby("customer_id")[
